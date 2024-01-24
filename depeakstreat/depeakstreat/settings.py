f6e330dd94c78e02ab0390f76b3dc9bfc0be31a4
Tomiwa-Flo <Rajiflowest@gmail.com>
2024-01-24 10:18:22 +0100

    Update static files configuration

--- depeakstreat/depeakstreat/settings.py
+++ depeakstreat/depeakstreat/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'de_peak',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
